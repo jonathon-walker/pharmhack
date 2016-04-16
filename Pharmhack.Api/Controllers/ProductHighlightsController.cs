4300632748d22c9ddc224c5d663fae5bafe4a6cd
Jalitha De Silva <jdes_10@hotmail.com>
2016-04-16 22:26:10 +1000

    product highlights skeleton

--- Pharmhack.Api/Controllers/ProductHighlightsController.cs
+++ Pharmhack.Api/Controllers/ProductHighlightsController.cs
@@ -4,10 +4,31 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Pharmhack.OData;
 
 namespace Pharmhack.Api.Controllers
 {
 	public class ProductHighlightsController : ApiController
 	{
+		readonly FredClient fred;
+
+        public ProductHighlightsController(FredClient fred)
+		{
+			this.fred = fred;
+		}
+
+		public IHttpActionResult Get()
+		{
+			var things = fred.RetailTransactionSalesTrans.Where(x => x.CostAmount > 0)
+				.Take(10)
+				.Select(x => new
+				{
+					x.CostAmount,
+					x.Price,
+					Difference = x.CostAmount - x.Price
+				});
+
+			return Ok(things);
+		}
 	}
 }
