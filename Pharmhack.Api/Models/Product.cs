cdedd6fde0826700b05704d440b4e2340c43f208
Jonathon Walker <jonathonwalker93@gmail.com>
2016-04-16 22:36:10 +1000

    WIP on RecommendedOrders, extension methods for fun and profit

---
+++ Pharmhack.Api/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pharmhack.Api.Models
+{
+	public class Product
+	{
+		public string Name { get; set; }
+		public string Sku { get; set; }
+		public string Description { get; set; }
+	}
+}
